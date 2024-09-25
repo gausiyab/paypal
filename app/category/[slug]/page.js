6d618b166cf5e85989ddcebb8c0de1e5ef64f4f4
Vikas Sankhla <admin@webcodder.dev>
2024-09-25 16:44:11 +0530

    bug fixes with category add

--- app/category/[slug]/page.js
+++ app/category/[slug]/page.js
@@ -1,11 +1,15 @@
 import Footer from "@/app/components/Footer";
 import Headers from "../../components/Headers"
+import { cateGory } from "@/lib/category";
 
 
 export default async function page({params}) {
   const {slug} = params;
 
+  const data = await cateGory(slug);
 
+  console.log(data);
+  
   
   return (
     <div>
