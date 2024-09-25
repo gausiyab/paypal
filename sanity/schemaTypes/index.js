9db057d00dc3e58492494a1cc98f3bdd37b29ba6
Mohd Nisar <133788768+gausiyab@users.noreply.github.com>
2024-09-25 16:27:38 +0530

    Update index.js
    update index schema

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -4,8 +4,9 @@ import product from "./product-schema"
 import order from "./order-schema";
 import comment from "./comment-schema";
 import contact from "./contact-schema";
+import category from "./category";
 
 // const schemas = [product,order,comment,contact];
-export const schemaTypes = [product,order,comment,contact]
+export const schemaTypes = [product,order,comment,contact,category]
 
 // export default schemas;
